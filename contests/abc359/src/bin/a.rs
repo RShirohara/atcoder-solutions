1260b1c9552ea33fec078084cb87dbd971cf6e5c
Ray Shirohara <RShirohara@proton.me>
2024-06-22 21:09:38 +0900

    feat: submit `abc359-a`

--- contests/abc359/src/bin/a.rs
+++ contests/abc359/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-  todo!();
+  input! {
+    n: usize,
+    s: [String; n]
+  }
+
+  let mut sum = 0;
+  for text in s.iter() {
+    if text == "Takahashi" {
+      sum += 1;
+    }
+  }
+  println!("{}", sum);
 }
