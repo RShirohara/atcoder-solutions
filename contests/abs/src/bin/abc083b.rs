ee375dc665567fa9b29118fefb92d340cebd1017
Ray Shirohara <RShirohara@proton.me>
2024-06-26 23:25:22 +0900

    feat: submit `abc083b`

--- contests/abs/src/bin/abc083b.rs
+++ contests/abs/src/bin/abc083b.rs
@@ -1,5 +1,23 @@
 use proconio::input;
 
 fn main() {
-  todo!();
+  input! {
+    n: usize,
+    a: usize,
+    b: usize
+  }
+  let mut sum = 0;
+
+  for n in 1..n + 1 {
+    let n_string = n.to_string();
+    let n_sum = n_string
+      .chars()
+      .map(|c| c.to_digit(10).unwrap() as usize)
+      .fold(0, |sum, x| sum + x);
+    if n_sum >= a && n_sum <= b {
+      sum += n;
+    }
+  }
+
+  println!("{}", sum);
 }
