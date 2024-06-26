36cc8a125f2f3abccdd3ccbb835b8a0e6887dd39
Ray Shirohara <RShirohara@proton.me>
2024-06-26 23:35:54 +0900

    feat: submit `abc085b`

--- contests/abs/src/bin/abc085b.rs
+++ contests/abs/src/bin/abc085b.rs
@@ -1,5 +1,21 @@
+use itertools::sorted;
 use proconio::input;
 
 fn main() {
-  todo!();
+  input! {
+    n: usize,
+    d: [usize; n]
+  }
+
+  let mut count = 0;
+  let mut before_x = 101;
+
+  for x in sorted(d.iter()).rev() {
+    if x < &before_x {
+      count += 1;
+    }
+    before_x = x.to_owned();
+  }
+
+  println!("{}", count);
 }
