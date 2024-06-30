09234e1381e0c44d352b2754ec7ae419602b7e4f
Ray Shirohara <RShirohara@proton.me>
2024-06-30 20:07:47 +0900

    feat: submit `abc088b`

--- contests/abs/src/bin/abc088b.rs
+++ contests/abs/src/bin/abc088b.rs
@@ -1,5 +1,22 @@
 use proconio::input;
 
 fn main() {
-  todo!();
+  input! {
+    n: usize,
+    a: [usize; n]
+  }
+
+  let mut cards = a.to_owned();
+  cards.sort();
+  cards.reverse();
+  let mut alices_score = 0;
+  for (i, card) in cards.iter().enumerate() {
+    if i % 2 == 0 {
+      alices_score += card;
+    } else {
+      alices_score -= card
+    }
+  }
+
+  println!("{}", alices_score);
 }
