b29a24550d79add8659b1b0802406cfd961d82d2
Ray Shirohara <RShirohara@proton.me>
2024-11-03 20:27:23 +0900

    feat: submit `c`

--- contests/adt_easy_20241029_1/src/bin/c.rs
+++ contests/adt_easy_20241029_1/src/bin/c.rs
@@ -1,5 +1,16 @@
 use proconio::input;
+use regex::Regex;
 
 fn main() {
-  todo!();
+  input! {
+    s: String
+  }
+
+  let pattern = Regex::new(r"^[A-Z][1-9][0-9]{5}[A-Z]$").unwrap();
+  let result = match pattern.is_match(&s) {
+      true => "Yes",
+      false => "No"
+  };
+
+  println!("{}", result)
 }
