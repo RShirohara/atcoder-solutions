3967253ccf115697550edfafdafa1f6194427bd0
Ray Shirohara <RShirohara@proton.me>
2024-11-03 19:49:37 +0900

    feat: submit `a`

--- contests/adt_easy_20241029_1/src/bin/a.rs
+++ contests/adt_easy_20241029_1/src/bin/a.rs
@@ -1,5 +1,14 @@
 use proconio::input;
 
 fn main() {
-  todo!();
+  input! {
+    a: [usize; 10]
+  }
+
+  let mut digit = 0;
+  for _ in 1..=3 {
+    digit = a[digit];
+  }
+
+  println!("{}", digit);
 }
