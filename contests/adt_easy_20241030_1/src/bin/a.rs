7a3cb75d01f5321f56551327ffda9de04a7df8c1
Ray Shirohara <RShirohara@proton.me>
2024-11-04 20:04:53 +0900

    feat: submit `a`

--- contests/adt_easy_20241030_1/src/bin/a.rs
+++ contests/adt_easy_20241030_1/src/bin/a.rs
@@ -1,5 +1,19 @@
 use proconio::input;
 
 fn main() {
-  todo!();
+  input! {
+    n: usize,
+    p: usize,
+    q: usize,
+    d: [usize; n],
+  }
+
+  let minimum_menu_amount = d.iter().min().unwrap();
+  let discounted_amount = minimum_menu_amount + q;
+  let result = match p < discounted_amount {
+    true => p,
+    false => discounted_amount,
+  };
+
+  println!("{}", result)
 }
