9f98a744d1162d1088d95d7ef610486de300fc8f
Ray Shirohara <RShirohara@proton.me>
2024-11-04 20:31:10 +0900

    feat: submit `d`

--- contests/adt_easy_20241030_1/src/bin/d.rs
+++ contests/adt_easy_20241030_1/src/bin/d.rs
@@ -1,5 +1,22 @@
 use proconio::input;
 
 fn main() {
-  todo!();
+  input! {
+    n: usize,
+    a: [usize; 4*n-1],
+  }
+
+  let mut numbers = vec![0; n];
+  for i in a {
+    numbers[i - 1] += 1;
+  }
+
+  let result = numbers
+    .iter()
+    .enumerate()
+    .min_by(|a, b| a.1.cmp(b.1))
+    .unwrap()
+    .0+1;
+
+  println!("{}", result)
 }
