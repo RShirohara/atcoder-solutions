20f10cf02b5b2e32e28f426b47691adbf419cfd4
Ray Shirohara <RShirohara@proton.me>
2024-11-04 21:00:00 +0900

    feat: submit `e` (failed)

--- contests/adt_easy_20241030_1/src/bin/e.rs
+++ contests/adt_easy_20241030_1/src/bin/e.rs
@@ -1,5 +1,19 @@
 use proconio::input;
 
 fn main() {
-  todo!();
+  input! {
+    n: usize,
+    q: usize,
+    a: [usize; n],
+    x: [usize; q],
+  }
+
+  let results = x
+    .iter()
+    .map(|j| a.iter().filter(|&i| j <= i).collect::<Vec<&usize>>().len())
+    .collect::<Vec<usize>>();
+
+  for r in results {
+    println!("{}", r)
+  }
 }
