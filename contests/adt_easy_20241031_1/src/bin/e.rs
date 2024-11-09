1b335f0bca0a6b7c5640f369a1a271998e572fe5
Ray Shirohara <RShirohara@proton.me>
2024-11-09 21:18:26 +0900

    feat: submit `e`  (explanation)

--- contests/adt_easy_20241031_1/src/bin/e.rs
+++ contests/adt_easy_20241031_1/src/bin/e.rs
@@ -1,5 +1,22 @@
 use proconio::input;
 
 fn main() {
-  todo!();
+  input! {
+    n: u32,
+    ab: [(u16, u16); n],
+  }
+
+  let mut t: f64 = 0.0;
+  let mut ans: f64 = 0.0;
+
+  for i in 0..n {
+    t += ab[i as usize].0 as f64 / ab[i as usize].1 as f64;
+  }
+  t /= 2.0;
+  for i in 0..n {
+    ans += (ab[i as usize].0 as f64).min(t * ab[i as usize].1 as f64);
+    t -= t.min(ab[i as usize].0 as f64 / ab[i as usize].1 as f64);
+  }
+
+  println!("{}", ans)
 }
